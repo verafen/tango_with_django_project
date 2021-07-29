3082524a8e1a904831bef9157959d1a1986b2530
fen yuan <2589593y@student.gla.ac.uk>
2021-07-29 23:47:10 +0100

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 
 # Add in this class to customise the Admin Interface
@@ -9,3 +10,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -37,3 +38,12 @@ class Page(models.Model):
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
